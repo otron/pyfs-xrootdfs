ef3ad7d08708967fede59a07cf76c7d3aba1fbc5
otron <oddweb42+gith@gmail.com>
2015-08-04 16:05:42 +0200

    v. basic constructor

--- src/XRootDFS/__init__.py
+++ src/XRootDFS/__init__.py
@@ -1,1 +1,9 @@
 # I guess this is how python projects work?
+
+import fs.base
+from XRootD import client
+
+class XRootDFS(fs.base.FS):
+    def __init__(self, addr, path='/'):
+        self.fs = client.FileSystem(addr)
+        self.path = path
