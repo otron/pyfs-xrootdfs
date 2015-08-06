48f0f3a3e3d2e0e716c0ecbfbd101f7d09a87d96
otron <oddweb42+gith@gmail.com>
2015-08-06 15:24:52 +0200

    add basic listdir implementation

--- src/XRootDFS/__init__.py
+++ src/XRootDFS/__init__.py
@@ -1,9 +1,13 @@
 # I guess this is how python projects work?
 
 import fs.base
-from XRootD import client
+from XRootD import client as xclient
 
 class XRootDFS(fs.base.FS):
     def __init__(self, addr, path='/'):
-        self.fs = client.FileSystem(addr)
-        self.path = path
+        self.fs = xclient.FileSystem(addr)
+        self.base_path = path
+
+    def listdir(self, path='./'):
+        status, res = self.fs.dirlist(self.base_path + path)
+        return [unicode(e.name) for e in res]
