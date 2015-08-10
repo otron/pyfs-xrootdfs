bd8cfaf1307dc3a5cd19616270ab4a43e4ca084c
otron <oddweb42+gith@gmail.com>
2015-08-10 09:59:25 +0200

    sure, the test still runs green

--- test/pyxrootd_test.py
+++ test/pyxrootd_test.py
@@ -8,7 +8,19 @@ cfg = ConfigObj('test/conf.cfg')['system']
 def xrootd_client():
     return client.FileSystem(cfg['address'])
 
+@pytest.fixture
+def base_path():
+    return cfg['home_dir'] + 'test/'
+
 class Test_Dirlist():
 
-    def test_what(self, xrootd_client):
-        pass
+    def test_dirlist(self, xrootd_client, base_path):
+        # get the dirlisting via pyxrootd
+        xdc = xrootd_client
+        status, listing = xdc.dirlist(base_path)
+
+        # this directory is initially empty
+        assert len(listing.dirlist) == 0
+        #for e in listing:
+            #print "{0} {1:>10} {2}".format(e.statinfo.modtimestr, entry.statinfo.size, entry.name)
+        assert 2 == 2
