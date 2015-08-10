2beb0eb6b7dab481fff14509129b73a5edb994a0
otron <oddweb42+gith@gmail.com>
2015-08-10 09:53:41 +0200

    add methods to be implemented

--- src/XRootDFS/xrdfile.py
+++ src/XRootDFS/xrdfile.py
@@ -1,4 +1,5 @@
 from XRootD import client
+from XRootD import File as XFile
 import xrootdfs
 import fs.filelike
 
@@ -6,3 +7,20 @@ class XRootDFile(fs.filelike.FileLikeBase):
 
     def __init__(self, bufsize=1024*64):
         super(XRootDFile, self).__init__(bufsize)
+
+    def seek(self, offset, whence=0):
+        self._ifp = offset
+
+    def read(self, size=0, offset=None, timeout=0, callback=None):
+        if offset=None:
+            offset=self._ifp
+        return self._file.read(offset, size, timeout, callback)
+
+    def tell():
+        pass
+
+    def truncate(size=None):
+        pass
+
+    def write(string):
+        pass
